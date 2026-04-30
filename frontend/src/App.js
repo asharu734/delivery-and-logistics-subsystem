d15d619e449d5f17eb3cb9541708f0e64abde9a1
spidergab <gabraposa@gmail.com>
2026-04-30 15:29:01 +0800

    renders the map component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,9 @@
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-} from "@vis.gl/react-google-maps";
+import MapComponent from "./components/Map";
 
 export default function App() {
-  const position = { lat: 14.0667, lng: 121.3239 };
-
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-        <Map
-          defaultCenter={position}
-          defaultZoom={14}
-          style={{ width: "100%", height: "100%" }}
-        >
-          <AdvancedMarker position={position}>
-            <Pin />
-          </AdvancedMarker>
-        </Map>
-      </APIProvider>
+    <div style={{ height: "100vh" }}>
+      <MapComponent />
     </div>
   );
 }
