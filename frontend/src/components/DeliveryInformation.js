ac5da747b1930da55ace432dab627139ea20b034
Azeri-ne <loudenhal@protonmail.com>
2026-05-04 13:51:53 +0800

    Change link

---
+++ frontend/src/components/DeliveryInformation.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function DeliveryList() {
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://customer-and-order-mgmt-system.vercel.app/api/orders')
+            .then(res => setOrders(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <div>
+            <h1>Deliveries</h1>
+            {orders.map(order => (
+                <div key={order._id}>Order for: {order.customerName}</div>
+            ))}
+        </div>
+    );
+}
