691e24288b3da8911550c5c93a0f36f0066a79ce
Azeri-ne <loudenhal@protonmail.com>
2026-05-06 14:23:29 +0800

    Add routes

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ app.get("/", (req, res) => {
   res.send("Delivery Status Update API is running");
 });
 
-app.use("/api/deliveries", require("./routes/DeliveryRoutes"));
+app.use("/api/orders", require("./routes/OrderRoute"))
+app.use("/api/deliveries", require("./routes/DeliveryRoute"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
