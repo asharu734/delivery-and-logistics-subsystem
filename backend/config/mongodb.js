04f6ad956f6aaca5109c8ab462b6d7fe906c8832
Azeri-ne <loudenhal@protonmail.com>
2026-05-10 14:09:38 +0800

    Wrap URI in String()

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(String(process.env.MONGO_URI));
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
     catch (err) {
         console.error(`MongoDB connection failed: ${err.message}`);
-        console.log("! No database persistence !");
+        console.log("! Could not connect to DB. No database persistence !");
     }
 };
 
