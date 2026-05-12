d8e51b91ce6cfee4590fa0e39088acae2c739af7
Azeri-ne <loudenhal@protonmail.com>
2026-05-13 07:39:04 +0800

    Change API link

--- frontend/react/src/components/Order.jsx
+++ frontend/react/src/components/Order.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import API from "../services/api";
+import API from "https://delivery-and-logistics-subsystem.onrender.com/api/deliveries";
 
 function Order() {
     const [deliveries, setDeliveries] = useState([]);
